360a9061862cc281cc9b2f4dc8ea52baacffa553
New Jio(Praveen) <praveennaikrupavath@praveens-macbook-air.local>
2018-06-08 02:23:27 -0500

    Initial Commit

---
+++ Four/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Four
+//
+//  Created by Praveen naik Rupavath on 08/06/18.
+//  Copyright © 2018 praveenn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
